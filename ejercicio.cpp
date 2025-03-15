80404983d1f5f87fd52d23d44a632345396e86f9
ClaraZb <czavaronibenoit@udesa.edu.ar>
2025-03-14 23:44:05 -0300

    termino ej 4b

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
+#include <chrono>
+
 using namespace std;
 
-//intentar con string y comparar el tiempo
-bool comparar(const char* palabra1, const char* palabra2){
+bool comparar(string palabra1, string palabra2){ //pasar como nuevo parametro i
+    if(palabra1.substr(0, 1) != palabra2.substr(0, 1)){
+        return false;
+    }
+    if (size(palabra1) == 1 && size(palabra2) == 1){
+        return true;
+    }
+    return comparar(palabra1.substr(1, size(palabra1)), palabra2.substr(1, size(palabra2)));
+}
+
+bool comparar(const char* palabra1, const char* palabra2){ //poner constexpr?
     if (palabra1 != palabra2){ 
         return false;
     }
@@ -15,7 +27,22 @@ bool comparar(const char* palabra1, const char* palabra2){
 int main(){
     const char *palabra1 = "El señor y la señora Dursley, que vivían en el número 4 de Privet Drive, estaban orgullosos.";
     const char *palabra2 = "El señor y la señora Dursley, que vivían en el número 5 de Privet Drive, estaban orgullosos.";
-    bool res = comparar(palabra1, palabra2);
-    cout << res << endl;
+    
+    auto startTime = chrono::high_resolution_clock::now();
+    comparar(palabra1, palabra2);
+    auto endTime = chrono::high_resolution_clock::now();
+    auto elapsedTime = chrono::duration_cast<chrono::nanoseconds>(endTime-startTime);
+    cout << "A comparar le tomo: " <<elapsedTime.count() << "nanosegundos" << endl;
+    
+
+    //string palabraA = "El señor y la señora Dursley, que vivían en el número 4 de Privet Drive, estaban orgullosos.";
+    //string palabraB = "El señor y la señora Dursley, que vivían en el número 5 de Privet Drive, estaban orgullosos.";
+    //
+    //auto startTime = chrono::high_resolution_clock::now();
+    //comparar(palabraA, palabraB);
+    //auto endTime = chrono::high_resolution_clock::now();
+    //auto elapsedTime = chrono::duration_cast<chrono::nanoseconds>(endTime-startTime);
+    //cout << "A comparar le tomo: " <<elapsedTime.count() << "nanosegundos" << endl;
+
     return 0;
 }
